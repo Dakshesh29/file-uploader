2a5593aa343f558e90afabc72ea9690958f14d78
Dakshesh Bijawar <bdaksheshbijawar735@gmail.com>
2025-09-02 15:34:35 +0530

    initialize express server and project structure

---
+++ index.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv";
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Server is up and running!");
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
+});
