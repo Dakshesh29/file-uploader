d9fb010c56590184eec05e736f8103708f110ec3
Dakshesh Bijawar <bdaksheshbijawar735@gmail.com>
2025-09-08 22:20:42 +0530

    fix redirect

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,36 +7,29 @@ export const registerUser = async (req, res) => {
   try {
     const { email, password } = req.body;
 
-    const existingUser = await prisma.user.findUnique({
-      where: { email },
-    });
+    const existingUser = await prisma.user.findUnique({ where: { email } });
 
     if (existingUser) {
-      return res
-        .status(400)
-        .json({ message: "User with this email already exists." });
+      return res.render("register", {
+        error: "User with this email already exists.",
+      });
     }
 
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
 
-    const user = await prisma.user.create({
+    await prisma.user.create({
       data: {
         email,
         password: hashedPassword,
       },
     });
 
-    const userResponse = { id: user.id, email: user.email };
-
-    res
-      .status(201)
-      .json({ message: "User created successfully", user: userResponse });
+    res.redirect("/login");
   } catch (error) {
-    res.status(500).json({ message: "Server error", error: error.message });
+    res.render("register", { error: "Server error. Please try again." });
   }
 };
-
 export const loginUser = (req, res) => {
   res.status(200).json({
     message: "Logged in successfully",
